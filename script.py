6d062da7a5842967b6ade309665ffed55308806d
Nardix <antonio.nardella98@outlook.com>
2024-09-15 14:54:19 +0200

    download from youtube

---
+++ script.py
@@ -0,0 +1,34 @@
+from pytubefix import YouTube 
+
+# where to save 
+SAVE_PATH = "C:/Users/Antonio/Desktop/riserva/pronto"
+
+# link of the video to be downloaded 
+links = []
+
+with open('links.txt', 'r') as file:
+    # Leggi tutte le righe e rimuovi eventuali spazi bianchi (come \n)
+    links = [line.strip() for line in file]
+
+for link in links:
+    try: 
+        # object creation using YouTube 
+        yt = YouTube(link) 
+    except: 
+        #to handle exception 
+        print("Connection Error") 
+
+    # Get all streams and filter for mp4 files
+    d_video = yt.streams.filter(file_extension='mp4').get_highest_resolution()
+
+    try: 
+        # downloading the video 
+        d_video.download(output_path=SAVE_PATH)
+        print('Video downloaded successfully!\n')
+    except: 
+        print("Some Error!\n")
+
+# Cancella il contenuto del file una volta finito
+with open('links.txt', 'w') as file:
+    # Scrivi una stringa vuota per cancellare il contenuto del file
+    file.write("")
